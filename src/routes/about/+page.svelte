5c6a3390b67c4d31504e6fb4dde74b2e3771979e
Rob Leroy <robleroy@msn.com>
2023-08-19 09:09:37 -0500

    update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -12,7 +12,7 @@
   <img src={Delores} alt="Delores River Bathing" class="delores" />
 </div>
 
-<p>Many topics are presented here with varying degrees of complexity.  Neither the depth nor the bredth of these topics are comprehensive.  It is more of a 'stream of consciousness', general thoughts on what a newcomer to rafting should begin to grasp.  If nothing more it is the first time you have heard some of these practices and principles.  They will become more embedded as you hear them again, consider them, and experience them more in the future.</p>
+<p>Many topics are presented here with varying degrees of complexity.  Neither the depth nor the bredth of these articles are comprehensive.  It is more a 'stream of consciousness', general thoughts on what a newcomer to rafting should begin to grasp.  If nothing more it is the first time you have heard some of these practices and principles.  They will become more embedded as you hear them again, consider them, and experience them more in the future.</p>
 
 
 <p style="font-style: italic; padding: 0 1em;"> ...the river is everywhere at once, at its origin and at its mouth, at the waterfall, at the ferry, at the rapids, in the sea, in the mountains, everywhere at the same time, and for it only the present exists, no shadow of the past, no shadow of the future. <br>
