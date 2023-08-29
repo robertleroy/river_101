6912c5c97d98e88d4cd1462369bb869ad79660ba
Rob Leroy <robleroy@msn.com>
2023-08-29 13:41:31 -0500

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,12 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import { mdsvex } from "mdsvex";
 
 const config = {
   kit: {
     adapter: adapter()
   },
-  extensions: [".svelte", ".svelte.md", ".md", ".svx"],
   preprocess: [
     vitePreprocess(),
-    mdsvex({
-      extensions: [".svelte.md", ".md", ".svx"],
-      smartypants: { dashes: "oldschool" },
-    }),
   ],
 };
 
